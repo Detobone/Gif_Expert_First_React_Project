5fffb8bb0ea57e5d4d39b1d64bcd9e75f46acb50
Detobone <detobone22@gmail.com>
2022-11-11 21:51:39 -0500

    First react project

---
+++ README.md
@@ -0,0 +1,12 @@
+# GifExpertApp
+
+This is a small project I have done in react. The repository was built by following Fernando Herrera's course 'React: De cero a experto(Hooks y MERN)' at UDEMY platform.
+
+During this project I learned the basics about how functional components are implemented and how they communicate with eachother by their properties; Also, how to use useState and useEffect hooks as well as creating a custom hook.
+
+The project was built using vite and was deployed in two different platforms netlify and GitHub Pages. If you want to check the website being deployed visit eather of the following links:
+
+Netlify:
+https://timely-haupia-48e99e.netlify.app
+
+GitHub Pages:
---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch"]);
+
+  const onAddCategory = (newCategory) => {
+    // I can add later a validation for case sensitive ej. ONE puch, one punch
+    if (categories.includes(newCategory)) return;
+    // setCategories([newCategory, ...categories]);
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory onNewCategory={onAddCategory} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
+
+/* The key is mandatory for dynamic elements like array.maps, etc */
